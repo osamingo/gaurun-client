50fbf62ab9aef2470a15074c226dfdd87a2b2950
Osamu TONOMORI <osamingo@gmail.com>
2017-06-29 11:51:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# gaurun-client
+
+[![Travis branch](https://img.shields.io/travis/osamingo/gaurun-client/master.svg)](https://travis-ci.org/osamingo/gaurun-client)
+[![codecov](https://codecov.io/gh/osamingo/gaurun-client/branch/master/graph/badge.svg)](https://codecov.io/gh/osamingo/gaurun-client)
+[![Go Report Card](https://goreportcard.com/badge/osamingo/gaurun-client)](https://goreportcard.com/report/osamingo/gaurun-client)
+[![codebeat badge](https://codebeat.co/badges/2a985c65-b562-4eb2-a7c5-82730283d678)](https://codebeat.co/projects/github-com-osamingo-gaurun-client-master)
+[![GoDoc](https://godoc.org/github.com/osamingo/gaurun-client?status.svg)](https://godoc.org/github.com/osamingo/gaurun-client)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/osamingo/gaurun-client/master/LICENSE)
+
+## About
+
+[Gaurun](https://github.com/mercari/gaurun) Client written in Golang.
+
+## Install
+
+```
+$ go get -u github.com/osamingo/gaurun-client
+```
+
+## License
+
+Released under the [MIT License](https://github.com/osamingo/gaurun-client/blob/master/LICENSE).
---
+++ struct.go
@@ -0,0 +1,47 @@
+package gaurun
+
+import "github.com/mercari/gaurun/gaurun"
+
+const (
+	// PlatformAndroid is enum for FCM/GCM.
+	PlatformAndroid Platform = gaurun.PlatFormAndroid
+	// PlatformIOS is enum for APNs.
+	PlatformIOS Platform = gaurun.PlatFormIos
+)
+
+type (
+	// A Payload has notifications.
+	Payload struct {
+		Notifications []*Notification `json:"notifications"`
+	}
+	// A Notification has gaurun notification data.
+	Notification struct {
+		Tokens   []string `json:"token"`
+		Platform Platform `json:"platform"`
+		Message  string   `json:"message"`
+		// Metadata
+		ID uint64 `json:"seq_id,omitempty"`
+		AndroidSetting
+		IOSSetting
+	}
+	// An AndroidSetting has setting fields for FCM/GCM.
+	AndroidSetting struct {
+		CollapseKey    string `json:"collapse_key,omitempty"`
+		DelayWhileIdle bool   `json:"delay_while_idle,omitempty"`
+		TimeToLive     int    `json:"time_to_live,omitempty"`
+	}
+	// An IOSSetting has setting fields for APNs.
+	IOSSetting struct {
+		Badge            int       `json:"badge,omitempty"`
+		Sound            string    `json:"sound,omitempty"`
+		ContentAvailable bool      `json:"content_available,omitempty"`
+		MutableContent   bool      `json:"mutable_content,omitempty"`
+		Expiry           int       `json:"expiry,omitempty"`
+		Retry            int       `json:"retry,omitempty"`
+		Extend           []*Extend `json:"extend,omitempty"`
+	}
+	// An Extend is alias gaurun.ExtendJSON.
+	Extend gaurun.ExtendJSON
+	// A Platform is alias gaurun platform enum.
+	Platform int
+)
